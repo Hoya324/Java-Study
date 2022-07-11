b255a01a7c36914f053c80b12f93371f5e176278
GYEONGHO NA <gangho324@naver.com>
2022-07-11 15:14:31 +0000

    Algorithm solving

--- 프로그래머스-카카오/프로그래머스[크레인 인형뽑기 게임].java
+++ 프로그래머스-카카오/프로그래머스[크레인 인형뽑기 게임].java
@@ -1,38 +1,42 @@
-// import java.util.*;
+import java.util.*;
 
-// class Solution {
+class Solution {
 
-//     public static int search(int N, int move, int[][] board) {
-//         int result = 0;
-//         for (int i = 0; i < N; i++) {
-//             if (board[i][move-1] != 0) {
-//                 result = board[i][move-1];
-//                 board[i][move-1] = 0;
-//                 break;
-//             }
-//         }
-//         return result;
-//     } 
+    public static int search(int N, int move, int[][] board) {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (board[i][move-1] != 0) {
+                result = board[i][move-1];
+                board[i][move-1] = 0;
+                break;
+            }
+        }
+        return result;
+    } 
     
-//     public int solution(int[][] board, int[] moves) {
+    public int solution(int[][] board, int[] moves) {
         
-//         int answer = 0;
-//         int N = board[0].length;
-//         ArrayList<Integer> basket = new ArrayList<>();
+        int answer = 0;
+        int N = board[0].length;
+        Stack<Integer> basket = new Stack<>();
         
-//         //System.out.println(N);
+        //System.out.println(N);
         
-//         for (int move : moves) {
-//             basket.add(search(N, move, board));
-
-//             if (basket.size() > 1 && basket.get(basket.size()-1) == basket.get(basket.size()-2)) {
-        
-//                 basket.remove(basket.size()-1);
-//                 basket.remove(basket.size()-1);
-//                 answer += 2;
-//             }
+        for (int move : moves) {
+            int temp = search(N, move, board);
+            if (temp != 0) {
+                basket.push(temp);
+            }
             
-//         }
-//         return answer;
-//     }
-// }
+
+            if (basket.size() > 1 && basket.peek() == basket.get(basket.size()-2)) {
+        
+                basket.pop();
+                basket.pop();
+                answer += 2;
+            }
+            
+        }
+        return answer;
+    }
+}
